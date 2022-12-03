0483a8508e69cee2d4a2d9452cb5fc9c466657d1
dheeraj13127 <dheerajkumar13127@gmail.com>
2022-12-03 19:29:14 +0530

    bug fix

--- client/src/redux/actions/auth.jsx
+++ client/src/redux/actions/auth.jsx
@@ -137,3 +137,9 @@ export const sendSpendNotification = (userId) => async (dispatch) => {
       console.log(err);
     });
 };
+
+export const userSignOut = (navigate) => async (dispatch) => {
+    toast.success("Successfully signed out !");
+    sessionStorage.setItem("userId", "");
+    navigate("/signin");
+  };
